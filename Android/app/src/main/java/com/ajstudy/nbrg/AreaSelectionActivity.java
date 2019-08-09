c9c35989c511a2bceacaa791477b05c4a1f8bc35
haemin <haemin001212@naver.com>
2019-08-09 17:09:46 +0900

    -스플래시 화면 추가 -아이콘 및 앱 이름 변경 -지역선택 화면 UI 개발

--- Android/app/src/main/java/com/ajstudy/nbrg/AreaSelectionActivity.java
+++ Android/app/src/main/java/com/ajstudy/nbrg/AreaSelectionActivity.java
@@ -1,13 +1,65 @@
 package com.ajstudy.nbrg;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.ToggleButton;
 
 public class AreaSelectionActivity extends AppCompatActivity {
+
+    String[] areas = {"서대문구", "동작구", "노원구", "중구", "난방", "정해민", "송하예", "니 소식"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area_selection);
+        LinearLayout tableLayout = findViewById(R.id.selection_area_table);
+        LinearLayout row1 = findViewById(R.id.tab1);
+        LinearLayout row2 = findViewById(R.id.tab2);
+        LinearLayout row3 = findViewById(R.id.tab3);
+
+        for (int i = 0; i < areas.length; i++) {
+
+            LayoutInflater li = getLayoutInflater();
+            ToggleButton button = (ToggleButton) li.inflate(R.layout.btn_selection, tableLayout, false);
+            button.setText(areas[i]);
+            button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if(isChecked){
+                        buttonView.setTextColor(getColor(getApplicationContext(),R.color.colorAccent));
+                    }else{
+                        buttonView.setTextColor(getColor(getApplicationContext(),R.color.black));
+                    }
+                }
+            });
+
+            switch (i % 3) {
+                case 0:
+                    row1.addView(button);
+                    break;
+                case 1:
+                    row2.addView(button);
+                    break;
+                case 2:
+                    row3.addView(button);
+                    break;
+            }
+        }
+
+    }
+    public static final int getColor(Context context, int id) {
+        final int version = Build.VERSION.SDK_INT;
+        if (version >= 23) {
+            return context.getResources().getColor(id,null);
+        } else {
+            return context.getResources().getColor(id);
+        }
     }
 }
--- Android/app/src/main/java/com/ajstudy/nbrg/SplashActivity.java
+++ Android/app/src/main/java/com/ajstudy/nbrg/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.ajstudy.nbrg;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),AreaSelectionActivity.class));
+                finish();
+            }
+        },3000);
+
+        
     }
 }
